6a70d6b016e803b17bbcd7df0211caa453669fd9
Sean Jordan <sjordan2@umbc.edu>
2019-08-01 18:08:13 -0400

    Initial Commit

---
+++ ID Scanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ID Scanner
+//
+//  Created by Sean Jordan on 8/1/19.
+//  Copyright © 2019 UMBC SGA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
